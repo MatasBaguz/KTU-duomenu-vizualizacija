63e890aeef82a45838acb336f5fc2ef056438d3d
MatasBaguz <matbag1@ktu.lt>
2023-04-15 19:01:39 +0300

    fix_as

--- laboratorinis/app/app.R
+++ laboratorinis/app/app.R
@@ -1,39 +1,39 @@
-  library(shiny)
-  library(tidyverse)
-  library(readr)
-  library(ggplot2)
-  
-  data = read.csv("../data/lab_sodra.csv")
-  prafiltruota = data[data$ecoActCode == "471900",]
-  
-  # UI 
-  ui = fluidPage(
-    titlePanel("471900 Kita mažmeninė prekyba nespecializuotose parduotuvėse"),
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("company_code", label = "Company code:", choices = unique(prafiltruota$code)),
-        actionButton("submit_button", "Submit")
-      ),
-      
-      mainPanel(
-        plotOutput("company_plot"),
-        tableOutput("table")
-      )
+library(shiny)
+library(tidyverse)
+library(readr)
+library(ggplot2)
+
+data = read.csv("../data/lab_sodra.csv")
+prafiltruota = data[data$ecoActCode == "471900",]
+
+# UI 
+ui = fluidPage(
+  titlePanel("471900 Kita mažmeninė prekyba nespecializuotose parduotuvėse"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("company_code", label = "Company code:", choices = unique(prafiltruota$code)),
+      actionButton("submit_button", "Submit")
+    ),
+    
+    mainPanel(
+      plotOutput("company_plot"),
+      tableOutput("table")
     )
   )
-  
-  #  Server logic 
-  server <- function(input, output, session) {
-    Graf = reactive({
-      req(input$company_code)
-      Filter %>% filter(code %in% input$company_code)
-    })
-    output$company_plot <- renderPlot({
-      g = ggplot(Graf(), aes(x = month, y = avgWage, group = name, color = name))
-      g + geom_line()
-    })
-    output$table = renderTable(Graf())
-  }
-  
-  # Run the application 
-  shinyApp(ui = ui, server = server)
+)
+
+#  Server logic 
+server <- function(input, output, session) {
+  Graf = reactive({
+    req(input$company_code)
+    prafiltruota %>% filter(code %in% input$company_code)
+  })
+  output$company_plot <- renderPlot({
+    g = ggplot(Graf(), aes(x = month, y = avgWage, group = name, color = name))
+    g + geom_line()
+  })
+  output$table = renderTable(Graf())
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
