ef9dada924dd0bb87e3c19b97cf40463b882ed46
MatasBaguz <128812745+MatasBaguz@users.noreply.github.com>
2023-04-15 10:09:00 +0300

    Update lab.R

---
+++ laboratorinis/R/lab.R
@@ -0,0 +1,53 @@
+##########################################
+library(readr)
+read.csv("C:/Users/matas/Documents/lab_sodra.csv")
+head(lab_sodra)
+library(dplyr)
+Filtras = filter(lab_sodra,ecoActCode==471900)
+head(Filtras)
+####################################
+#1 uzduotis#
+hist(Filtras$avgWage, xlab = "Average Wage", ylab= "Frequency", breaks =20, col = "steelblue", main = "Average Wage")
+#################################################
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(tidyr)
+#2 uzduotis#
+Top5 = Filtras %>%
+  group_by(name)%>%
+  slice_max(avgWage, n=1)%>%
+  ungroup()%>%
+  top_n(avgWage, n=5)
+
+Top51 <- Filtras %>%
+  filter(name %in% Top5$name)
+
+ggplot(Top51, aes(x=Top51$month, y=Top51$avgWage))+geom_quantile()+
+  theme_minimal()
+
+ggplot(Top51, aes(x=month, y=avgWage, color=name)) +
+  geom_quantile() +
+  scale_color_manual(values = c("red", "blue", "green", "purple", "gold")) +
+  labs(x = "Month", y = "Average Wage", title = "Average Wage by Month for Top 5 Companies") +
+  theme_minimal() +
+  theme(legend.position = "right", 
+        legend.title = element_blank(),
+        legend.text = element_text(size = 12),
+        axis.title = element_text(size = 14))
+
+###################################################
+#3 uzduotis#
+
+Top51 %>%
+  group_by(name)%>%
+  slice_max(numInsured,n=1)%>%
+  top_n(numInsured, n=5)
+
+ggplot(Top52, aes(x=reorder(name, -numInsured), y=numInsured))+geom_col(fill = "steelblue") +
+  theme_minimal()+
+  labs(title = "Number of insured employees", x="Company", Y="Count")
+
+
+######################################################################
+
